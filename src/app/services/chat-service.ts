8b03cccbe8c9b9b1d0edb04bb95fb17051341ebf
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-01-04 14:21:18 +0530

    initChatStoreForUserId added functionality for method

--- src/app/services/chat-service.ts
+++ src/app/services/chat-service.ts
@@ -2,8 +2,7 @@ import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { combineLatest, Subject } from "rxjs";
-import { userModel } from "../type-definitions/authentication";
-import {User} from "firebase/auth"
+import { chatModel, userModel } from "../type-definitions/authentication";
 import { take } from "rxjs/operators";
 import { handleErrorMessage } from "../common-functions";
 
@@ -18,13 +17,22 @@ export class chatService{
     async initChatStoreForUserId(uid:string) : Promise<void>{
         try{
             const currentUserDoc=this.fireStore.collection("users").doc((await this.fireStoreAuth.currentUser)?.uid);
-        const otherUserDoc=this.fireStore.collection("users").doc(uid);
+            const otherUserDoc=this.fireStore.collection("users").doc(uid);
 
-        const collection1=this.fireStore.collection("messages",ref => ref.where("from","==",currentUserDoc.ref).where("to","==",otherUserDoc.ref));
-        const collection2=this.fireStore.collection("messages",ref => ref.where("from","==",otherUserDoc.ref).where("to","==",currentUserDoc.ref));
+            const collection1=this.fireStore.collection("messages",ref => ref.where("from","==",currentUserDoc.ref).where("to","==",otherUserDoc.ref));
+            const collection2=this.fireStore.collection("messages",ref => ref.where("from","==",otherUserDoc.ref).where("to","==",currentUserDoc.ref));
 
-        const docs =await combineLatest([collection1.valueChanges(),collection2.valueChanges()]).pipe(take(1)).toPromise();
-        console.log(docs);
+            const docs : any[] =await combineLatest([collection1.valueChanges(),collection2.valueChanges()]).pipe(take(1)).toPromise();
+            const toSend: {id:string,chat:chatModel[]}={id:otherUserDoc.ref.id,chat:[]};
+            
+            docs[0].forEach(((data: chatModel)  => {
+               toSend.chat.push(data);
+            }));
+
+            docs[1].forEach(((data: chatModel) => {
+                toSend.chat.push(data);
+            }));
+            console.log(toSend);
         }
         catch(e:any){
             handleErrorMessage(e.message);

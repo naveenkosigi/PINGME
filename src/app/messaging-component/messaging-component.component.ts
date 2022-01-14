ed4d3d8fde891f6139aede743bc8db55ea9b7955
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-01-14 16:13:25 +0530

    added scroll into view behavior after clicking user

--- src/app/messaging-component/messaging-component.component.ts
+++ src/app/messaging-component/messaging-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, ElementRef, OnChanges, OnDestroy, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Store } from '@ngrx/store';
 import { Observable, Subscription } from 'rxjs';
 import { chatState } from '../store/action-reducers';
@@ -10,17 +11,20 @@ import { chatModel } from '../type-definitions/authentication';
   templateUrl: './messaging-component.component.html',
   styleUrls: ['./messaging-component.component.css']
 })
-export class MessagingComponentComponent implements OnInit,OnDestroy {
+export class MessagingComponentComponent implements OnInit,OnDestroy,AfterViewInit {
 
   private chatObservable:Observable<chatState>;
   private chatSubscription:Subscription=new Subscription();
   public chats:chatModel[]=[];
   public fromUser:String="";
-  constructor(private store:Store<appState>) { 
+  private currentLoggedInuser : string | undefined;
+  private mutationObserver:MutationObserver | undefined;
+  @ViewChild('threadContainer') threadContainer: ElementRef | undefined;
+  constructor(private store:Store<appState>,private fireStoreAuth:AngularFireAuth) { 
     this.chatObservable=this.store.select('chat');
   }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
     this.chatSubscription=this.chatObservable.subscribe((data : chatState) => {
       console.log(data);
       if(data.currentUser !== null){
@@ -28,10 +32,24 @@ export class MessagingComponentComponent implements OnInit,OnDestroy {
         this.fromUser=data.currentUser;
       }
     });
+
+    this.currentLoggedInuser=(await this.fireStoreAuth.currentUser)?.uid;
+    console.log(this.currentLoggedInuser);
   }
 
   ngOnDestroy(): void {
       this.chatSubscription.unsubscribe();
   }
 
+  ngAfterViewInit(): void {
+      const targetNode = this.threadContainer?.nativeElement;
+      const config = { childList: true, subtree: true };
+
+      this.mutationObserver=new MutationObserver(() => {
+        const nodes=this.threadContainer?.nativeElement.querySelectorAll(".chat");
+        const lastNode=nodes[nodes.length-1];
+        lastNode.scrollIntoView({behavior : 'smooth'});
+      });
+      this.mutationObserver.observe(targetNode,config);
+  }
 }

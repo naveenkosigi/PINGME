b3c2189b28ae3ef7e1e14dc9c1c6dc85c856aad3
NAVEEN KOSGI <naveenkosigi@gmail.com>
2022-01-15 10:19:13 +0530

    chat selected toggle support added

--- src/app/chat-click.directive.ts
+++ src/app/chat-click.directive.ts
@@ -9,10 +9,15 @@ export class ChatClickDirective {
     this.toggleClass();
   }
 
+
   constructor(private elementRef:ElementRef) { 
   }
 
   private toggleClass(){
+    const elements:NodeList=document.querySelectorAll('div.chat-container.selected');
+    elements.forEach((element) => {
+      (element as HTMLElement).classList.remove("selected");
+    });
     (this.elementRef.nativeElement as HTMLElement).classList.add('selected');
   }
 
